0308cef71f8bc92e47e2d7d6805e1af4f50e96a4
Ketsup21 <163226460+Ketsup21@users.noreply.github.com>
2024-04-18 16:45:29 +0800

    Add files via upload

---
+++ ketsap/contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+        body{
+            background-color: gray;
+        }
+        h1{
+            text-align: center;
+            background-color: purple;
+        }
+        h2{
+            text-align: center;
+            color: orange;
+        }
+        h3{
+            text-align: center;
+            color: violet;
+        }
+        </style>
+        
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contact Us</h1>
+    <h2>TASTING LOUNGE
+HOURS</h2>
+<h3>10:00am - 4:30pm</h3>
+<h3>Reservations Required</h3>
+
+<h2>RESERVATIONS</h2>
+<h3>877.283.5934 or experiences@theprisonerwinecompany.com</h3>
+
+<h2>ADDRESS</h2>
+<h3>1178 Galleron Road</h3>
+<h3>St. Helena, CA 94574</h3>
+
+<h2>Find Us on Google Maps</h2>
+</body>
+</html>
---
+++ ketsap/quality.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quality</title>
+    <style>
+        body{
+            background-color: gray;
+        }
+        h1{
+            text-align: center;
+            background-color: purple;
+        }
+        h3{
+            text-align: center;
+            color: cyan;
+        }
+        </style>
+</head>
+<body>
+    <h1>Quality of our Wine</h1><br>
+    <h3>This is why the best easy to find the right wine for you, is to understand your preferences. To do this, you need to understand the five main characteristics of wine: sweetness, acidity, tannin, alcohol, and body.</h3>
+</body>
+</html>
